e1a9526781c1245ee7e4a541e0f8a6f18ca47382
FlorientPlouvin <fplouvin@itroom.fr>
2024-12-15 14:33:31 +0100

    chore: add tailwind

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,5 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@nuxtjs/i18n']
+  modules: ['@nuxtjs/i18n', '@nuxtjs/tailwindcss']
 })
